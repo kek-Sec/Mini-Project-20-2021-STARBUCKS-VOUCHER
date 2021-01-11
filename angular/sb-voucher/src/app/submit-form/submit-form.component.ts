c4211d1c277485a31f96ebced0a35d8c75d616e5
kek-Sec <georgepetrakis99@gmail.com>
2021-01-12 01:19:28 +0200

    submit form work

--- angular/sb-voucher/src/app/submit-form/submit-form.component.ts
+++ angular/sb-voucher/src/app/submit-form/submit-form.component.ts
@@ -1,18 +1,19 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ReCaptcha2Component } from 'ngx-captcha';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-
-
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-submit-form',
   templateUrl: './submit-form.component.html',
-  styleUrls: ['./submit-form.component.sass']
+  styleUrls: ['./submit-form.component.sass'],
 })
 export class SubmitFormComponent implements OnInit {
-
+  //form group stuff
   protected fg: FormGroup;
+  fg_init = false;
+  //end of form group stuff
+
 
   //captcha stuff
   public captchaIsLoaded = false;
@@ -25,20 +26,30 @@ export class SubmitFormComponent implements OnInit {
   public lang = 'en';
   public type: 'image' | 'audio';
   @ViewChild('captchaElem') captchaElem: ReCaptcha2Component; //captcha element
-  @ViewChild('langInput') langInput: ElementRef;  //captcha lang
+  @ViewChild('langInput') langInput: ElementRef; //captcha lang
   //end of captcha stuff
-  
 
-  //add @ViewChild('title', { static: true }) title: ElementRef;
-
-
-
-
-
-  constructor(private formBuilder: FormBuilder,private modalService: NgbModal) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.showModal();
+  }
+
+  //init form data
+  init_FormData() {
+    if (!this.fg_init) {
+      this.fg = this.formBuilder.group({
+        amka: [, Validators.required],
+        afm: [, Validators.required],
+        oaed_card: [, Validators.required],
+        at: [, Validators.required],
+        recaptcha: ['', Validators.required],
+      });
+      this.fg_init = true;
+    }
   }
 
   //captcha function for success
@@ -47,16 +58,19 @@ export class SubmitFormComponent implements OnInit {
   }
 
   //autoshow modal
-  showModal()
-  {
-    let element:HTMLElement = document.getElementById('open_modal') as HTMLElement;
+  showModal() {
+    let element: HTMLElement = document.getElementById(
+      'open_modal'
+    ) as HTMLElement;
     element.click();
   }
   //open modal
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+    let ngbModalOptions: NgbModalOptions = {
+      backdrop : 'static',
+      keyboard : false
+};
+    this.init_FormData();
+    this.modalService.open(content, ngbModalOptions);
   }
-
-  
-
 }
