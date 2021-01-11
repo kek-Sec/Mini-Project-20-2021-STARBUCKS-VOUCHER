30a8023e40994b3f08db50fec6799bffb655148e
kek-Sec <georgepetrakis99@gmail.com>
2021-01-11 23:51:29 +0200

    added components

--- angular/sb-voucher/src/app/app.module.ts
+++ angular/sb-voucher/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { SubmitFormComponent } from './submit-form/submit-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RedeemComponent } from './redeem/redeem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SubmitFormComponent,
+    DashboardComponent,
+    RedeemComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/sb-voucher/src/app/redeem/redeem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redeem',
+  templateUrl: './redeem.component.html',
+  styleUrls: ['./redeem.component.sass']
+})
+export class RedeemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
