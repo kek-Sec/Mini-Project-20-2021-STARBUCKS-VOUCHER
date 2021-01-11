a9ea8d012b3a140d29f983e8cc8c9d7267e9f2d8
kek-Sec <georgepetrakis99@gmail.com>
2021-01-11 23:24:22 +0200

    work on backend

---
+++ starbucks-rest/models/users_item.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const users_itemSchema = mongoose.Schema({
+    lastname: {
+        type: String,
+        required: true
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    father_name: {
+        type: String,
+        required: true
+    },
+    DOB: {
+        type: Date,
+        required: true
+    },
+    AFM : {
+        type: Number,
+        required: true
+    },
+    AMKA : {
+        type: Number,
+        required: true
+    },
+    CARD_ID : {
+        type: Number,
+        required: true
+    },
+    UNEMPLOYMENT_TIME : {
+        type: Number,
+        required: true
+    },
+    STATUS : {
+        type: Boolean,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('users_items', users_itemSchema);
